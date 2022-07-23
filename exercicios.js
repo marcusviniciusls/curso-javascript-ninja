db6359b482b97751f1b60dfce6dd594efd58c7e7
Marcus Vinicius Lima Silva <viniciusmls@outlook.com>
2022-07-23 16:07:07 -0300

    exercicios primeira semana

---
+++ exercicios.js
@@ -0,0 +1,18 @@
+var myvar = 10;
+var soma = 15 + 8;
+console.log(soma);
+soma++;
+console.log(soma);
+soma *= 3;
+console.log(soma);
+// Valor da várivavel soma é 72
+var souNinja = true;
+var comida = ["arroz", "feijao", "ovo"]
+console.log(comida[1]);
+console.log(soma == myvar);
+console.log(soma === myvar);
+console.log(myvar <= soma);
+function divisao(var1, var2){
+    return var1 / var2;
+}
+console.log(divisao(10,2));
